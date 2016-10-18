d4f05539ce9cedd7675c1badf604b8fe0e4f8156
bipin <bipin@bipin-lap2>
2016-10-18 22:34:27 +0530

    operation with operator

--- gdal_raster_operator.py
+++ gdal_raster_operator.py
@@ -13,25 +13,28 @@ def operation1(b1,b2,b3):
     b_out = (b1+b2)/b3
     return b_out
     
-def get_raster_band(file_name):    
+def get_raster_band(file_name):
+    'get band 1 from each file as a numpy array'''    
     f1 = gdal.Open(file_name,GA_ReadOnly)
     b1 = f1.GetRasterBand(1)    
     band_data = b1.ReadAsArray()
     return band_data,f1
     
-def write_raster_band(filename,data1,transform,srs):    
+def write_raster_band(filename,data1,transform,crs):    
+    '''write data1 to file with filename filename , geotransform transform and projection crs'''
     data1 = np.array(data1)
     (rows,cols) = np.shape(data1)         
     driver = gdal.GetDriverByName('GTiff')
     outRaster = driver.Create(filename, cols, rows, 1, gdal.GDT_Byte)    
     outRaster.SetGeoTransform(transform)
-    outRaster.SetProjection(srs)    
+    outRaster.SetProjection(crs)    
     outband = outRaster.GetRasterBand(1)    
     outband.WriteArray(np.array(data1)) 
     outband.FlushCache()
     
     
 def operator(operation,b1,b2,b3):
+    '''applay operator along rows and coloums of the data'''
     return map(lambda x1,x2,x3 : map(operation,x1,x2,x3),b1,b2,b3)
     
     
