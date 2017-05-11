c56fe616f839ad33162564ab5db4ed2d13a7096b
Bipin C <vu3bpn@gmail.com>
2017-05-11 06:38:00 +0530

    added Read Blocks

---
+++ Read_blocks.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 21:06:03 2017
+
+@author: bipin
+"""
+from osgeo import gdal
+from osgeo import osr
+from osgeo.gdalconst import *
+import numpy as np
+import itertools as it
+import struct
+
+in_fn1 = 'BAND2.tif'
+in_fn2 = 'BAND3.tif'
+out_fn = 'out_band.tif'
+
+if __name__ == '__main__':
+    f1 = gdal.Open(in_fn1,GA_ReadOnly)
+    f2 = gdal.Open(in_fn2,GA_ReadOnly)
+    driver = f1.GetDriver()
+    f_out = driver.CreateCopy( out_fn , f1, 0 )
+    f_out = None
+    f_out = gdal.Open(out_fn,GA_Update)
+    b1 = f1.GetRasterBand(1)
+    b2 = f2.GetRasterBand(1)
+    out_b1 = f_out.GetRasterBand(1)
+    [nx,ny] = b1.GetBlockSize()
+    for blk_idx_x,blk_idx_y in it.product(range(b1.XSize/nx),range(b1.YSize/ny)):
+        if b1.DataType == GDT_UInt16:
+            blk1 = b1.ReadBlock(blk_idx_x,blk_idx_y)
+            blk2 = b2.ReadBlock(blk_idx_x,blk_idx_y)
+            dat1 = np.array(struct.unpack('H'*nx*ny,blk1))
+            dat2 = np.array(struct.unpack('H'*nx*ny,blk2))
+            #dat1.shape = [nx,ny]
+            #dat2.shape = [nx,ny]
+            out = (dat2/2 + dat1/2)
+            packed_data = out.astype(np.uint16).tostring()
+            out_b1.WriteRaster(blk_idx_x*nx,blk_idx_y*ny,nx,ny,packed_data)
+            out_b1.FlushCache()
+            
+            
+    
