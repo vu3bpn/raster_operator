ef41937b584389a0c6a160be21b1965f1adcf0d5
Bipin C <vu3bpn@users.noreply.github.com>
2017-05-11 09:46:36 +0530

    made modular

--- Read_blocks.py
+++ Read_blocks.py
@@ -16,6 +16,28 @@ in_fn1 = 'BAND2.tif'
 in_fn2 = 'BAND3.tif'
 out_fn = 'out_band.tif'
 
+
+    
+    
+def operate_blocks(b1,b2,out_b1,operation):
+    [nx,ny] = b1.GetBlockSize()
+    for blk_idx_x,blk_idx_y in it.product(range(b1.XSize/nx),range(b1.YSize/ny)):
+        if b1.DataType == GDT_UInt16:
+            blk1 = b1.ReadBlock(blk_idx_x,blk_idx_y)
+            blk2 = b2.ReadBlock(blk_idx_x,blk_idx_y)
+            dat1 = np.array(struct.unpack('H'*nx*ny,blk1))
+            dat2 = np.array(struct.unpack('H'*nx*ny,blk2))
+            #dat1.shape = [nx,ny]
+            #dat2.shape = [nx,ny]
+            #out = (dat2/2 + dat1/2)
+            out = map(operation,dat1,dat2)
+            packed_data = out.astype(np.uint16).tostring()
+            out_b1.WriteRaster(blk_idx_x*nx,blk_idx_y*ny,nx,ny,packed_data)
+            out_b1.FlushCache()
+
+def operation(d1,d2):
+    return d1/2+d2/2
+
 if __name__ == '__main__':
     f1 = gdal.Open(in_fn1,GA_ReadOnly)
     f2 = gdal.Open(in_fn2,GA_ReadOnly)
@@ -26,19 +48,6 @@ if __name__ == '__main__':
     b1 = f1.GetRasterBand(1)
     b2 = f2.GetRasterBand(1)
     out_b1 = f_out.GetRasterBand(1)
-    [nx,ny] = b1.GetBlockSize()
-    for blk_idx_x,blk_idx_y in it.product(range(b1.XSize/nx),range(b1.YSize/ny)):
-        if b1.DataType == GDT_UInt16:
-            blk1 = b1.ReadBlock(blk_idx_x,blk_idx_y)
-            blk2 = b2.ReadBlock(blk_idx_x,blk_idx_y)
-            dat1 = np.array(struct.unpack('H'*nx*ny,blk1))
-            dat2 = np.array(struct.unpack('H'*nx*ny,blk2))
-            #dat1.shape = [nx,ny]
-            #dat2.shape = [nx,ny]
-            out = (dat2/2 + dat1/2)
-            packed_data = out.astype(np.uint16).tostring()
-            out_b1.WriteRaster(blk_idx_x*nx,blk_idx_y*ny,nx,ny,packed_data)
-            out_b1.FlushCache()
-            
-            
+    operate_blocks(b1,b2,out_b1,operation)
+                
     
